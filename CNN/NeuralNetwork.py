03e7f4836e5c667d90f498efedde5b0017833709
N.Bachour <nb425@tesla.kent.ac.uk>
2017-06-23 17:03:52 +0100

    Training graph setup. Needs testing

--- CNN/NeuralNetwork.py
+++ CNN/NeuralNetwork.py
@@ -9,45 +9,95 @@ class NeuralNetwork:
     def __init__(self, vector_dimention, epochs):
         ### Network settings ###
         self.vector_dimention = vector_dimention
-        self.epochs = epochs
+        self.epochs = epochs # DEBUG : unused ?
 
         ### Creating weights ###
         # Create 100 kernels of 3 * k, 100 kernels of 4 * k, 100 kernels of 5 * k
         self.kernels = {
-            3 : tf.Variable(self.__shape_to_variables([1, 3 * self.vector_dimention, 100]), tf.float32),
-            4 : tf.Variable(self.__shape_to_variables([1, 4 * self.vector_dimention, 100]), tf.float32),
-            5 : tf.Variable(self.__shape_to_variables([1, 5 * self.vector_dimention, 100]), tf.float32)
+            3 : tf.Variable(self.__shape_to_variables([3 * self.vector_dimention, 100]), tf.float32),
+            4 : tf.Variable(self.__shape_to_variables([4 * self.vector_dimention, 100]), tf.float32),
+            5 : tf.Variable(self.__shape_to_variables([5 * self.vector_dimention, 100]), tf.float32)
         }
 
         # Create 300 * 2 weights for the output layer (2 classes : positive + negative)
         self.hidden_layer_weights = tf.Variable(self.__shape_to_variables([300, 2]), tf.float32)
 
         ### Build network graph ###
-        self.convoluted_input_3 = tf.placeholder(tf.float32, shape = [None, 1, 3 * self.vector_dimention]) # of shape (n - h + 1), 1, hk
-        self.convoluted_input_4 = tf.placeholder(tf.float32, shape = [None, 1, 4 * self.vector_dimention]) # of shape (n - h + 1), 1, hk
-        self.convoluted_input_5 = tf.placeholder(tf.float32, shape = [None, 1, 5 * self.vector_dimention]) # of shape (n - h + 1), 1, hk
+        self.convoluted_input_3 = tf.placeholder(tf.float32, shape = [None, None, 1, 3 * self.vector_dimention]) # of shape batches, (n - h + 1), 1, hk
+        self.convoluted_input_4 = tf.placeholder(tf.float32, shape = [None, None, 1, 4 * self.vector_dimention]) # of shape batches, (n - h + 1), 1, hk
+        self.convoluted_input_5 = tf.placeholder(tf.float32, shape = [None, None, 1, 5 * self.vector_dimention]) # of shape batches, (n - h + 1), 1, hk
 
         # Produce feature maps (of shape [k - s + 1, 100, 300])
-        convolution_3_feature_map = tf.tanh(tf.matmul(self.convoluted_input_3, tf.tile(self.kernels[3], [tf.shape(self.convoluted_input_3)[0], 1, 1]))) # of shape [n - h + 1, 1, 100]
-        convolution_4_feature_map = tf.tanh(tf.matmul(self.convoluted_input_4, tf.tile(self.kernels[4], [tf.shape(self.convoluted_input_4)[0], 1, 1]))) # of shape [n - h + 1, 1, 100]
-        convolution_5_feature_map = tf.tanh(tf.matmul(self.convoluted_input_5, tf.tile(self.kernels[5], [tf.shape(self.convoluted_input_5)[0], 1, 1]))) # of shape [n - h + 1, 1, 100]
+        convolution_3_feature_map = tf.tanh(tf.matmul(self.convoluted_input_3, tf.tile(tf.expand_dims(tf.expand_dims(self.kernels[3], 0), 0), [tf.shape(self.convoluted_input_3)[0], tf.shape(self.convoluted_input_3)[1], 1, 1]))) # of shape [batches, n - h + 1, 1, 100]
+        convolution_4_feature_map = tf.tanh(tf.matmul(self.convoluted_input_4, tf.tile(tf.expand_dims(tf.expand_dims(self.kernels[4], 0), 0), [tf.shape(self.convoluted_input_4)[0], tf.shape(self.convoluted_input_4)[1], 1, 1]))) # of shape [batches, n - h + 1, 1, 100]
+        convolution_5_feature_map = tf.tanh(tf.matmul(self.convoluted_input_5, tf.tile(tf.expand_dims(tf.expand_dims(self.kernels[5], 0), 0), [tf.shape(self.convoluted_input_5)[0], tf.shape(self.convoluted_input_5)[1], 1, 1]))) # of shape [batches, n - h + 1, 1, 100]
 
         # Max overtime pooling
-        feature_3 = tf.reduce_max(convolution_3_feature_map, reduction_indices = [0]) # of size [1, 100]
-        feature_4 = tf.reduce_max(convolution_4_feature_map, reduction_indices = [0]) # of size [1, 100]
-        feature_5 = tf.reduce_max(convolution_5_feature_map, reduction_indices = [0]) # of size [1, 100]
+        feature_3 = tf.reduce_max(convolution_3_feature_map, reduction_indices = [1]) # of size [batches, 1, 100]
+        feature_4 = tf.reduce_max(convolution_4_feature_map, reduction_indices = [1]) # of size [batches, 1, 100]
+        feature_5 = tf.reduce_max(convolution_5_feature_map, reduction_indices = [1]) # of size [batches, 1, 100]
+        # DEBUG : Sees tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')    for alternative
 
         # Output
-        # Concatenate along axis 1
-        feature_vector = tf.concat(1, [feature_3, feature_4, feature_5]) # of size [1, 300]
-        output_without_softmax = tf.matmul(feature_vector, self.hidden_layer_weights) # of size [2, 1]        
+        # Concatenate along axis 2
+        feature_vector = tf.concat(2, [feature_3, feature_4, feature_5]) # of size [batches, 1, 300]
+        output_without_softmax = tf.matmul(feature_vector, tf.tile(tf.expand_dims(self.hidden_layer_weights, 0), [tf.shape(self.convoluted_input_3)[0], 1, 1])) # of size [batches, 2, 1]
         self.output = tf.nn.softmax(output_without_softmax)
+        return
 
+    def set_training_set(self, training_set, target_labels, batch_size):
+        # training_set is a table containing inputs
+        # target_labels is a table containing target outputs
+
+        number_of_classes = len(target_labels[0]) # number of classes
+        # batch_size : number of trials
+        # target_labels : vector of length N with values corresponding to the correct class
 
         # Hand down the weights update to Adadelta
+        #indicators = np.zeros((batch_size, number_of_classes))
+        #indicators[np.arange(batch_size), target_labels] = 1
+
+        #output_matrix = []
+        #for i in xrange(0, batch_size):
+        #    output_matrix.append(self.output())
+
+        self.loss_hidden_layer = - tf.reduce_sum(tf.multiply(target_labels, tf.log(self.output)))
+
+        #loss_hidden_layer = tf.losses.softmax_cross_entropy(target_class, self.hidden_layer_weights)
+        #loss_convolution = {
+        #    3 : tf.losses.sigmoid_cross_entropy(un_truc_louche, self.kernels[3], weights = blame),
+        #    4 : tf.losses.sigmoid_cross_entropy(un_truc_louche, self.kernels[4], weights = blame),
+        #    5 : tf.losses.sigmoid_cross_entropy(un_truc_louche, self.kernels[5], weights = blame)
+        #}
+
         self.optimiser = tf.train.AdadeltaOptimizer(learning_rate = 1.0)
+        self.optimize_step = [
+            self.optimiser.minimize(self.loss_hidden_layer, var_list=[self.hidden_layer_weights]),#, aggregation_method=tf.AggregationMethod.ADD_N, reduction=tf.losses.Reduction.SUM)
+            #self.optimiser.minimize(self.loss_convolution[3], var_list=[self.convoluted_input_3]),#, aggregation_method=tf.AggregationMethod.ADD_N, reduction=tf.losses.Reduction.SUM)
+            #self.optimiser.minimize(self.loss_convolution[4], var_list=[self.convoluted_input_4]),#, aggregation_method=tf.AggregationMethod.ADD_N, reduction=tf.losses.Reduction.SUM)
+            #self.optimiser.minimize(self.loss_convolution[5], var_list=[self.convoluted_input_5])#, aggregation_method=tf.AggregationMethod.ADD_N, reduction=tf.losses.Reduction.SUM)
+        ]
+
+        convoluted_input_3_values = []
+        convoluted_input_4_values = []
+        convoluted_input_5_values = []
+        for sample in training_set:
+            processed_input = self.__preprocess_input(sample)
+            convoluted_input_3_values.append(processed_input[3])
+            convoluted_input_4_values.append(processed_input[4])
+            convoluted_input_5_values.append(processed_input[5])
 
         self.global_initialiser = tf.global_variables_initializer()
+        with tf.Session() as session:
+            session.run(self.global_initialiser) # DEBUG ?
+            results = session.run(self.optimize_step, feed_dict = {
+                self.convoluted_input_3 : convoluted_input_3_values,
+                self.convoluted_input_4 : convoluted_input_4_values,
+                self.convoluted_input_5 : convoluted_input_5_values
+            })
+            print(results)
+
+        #fc1 = tf.nn.dropout(fc1, dropout)
         return
 
     def run(self, input):
@@ -89,9 +139,9 @@ class NeuralNetwork:
                                                                                     #print(results)
             results = session.run(self.output, feed_dict=
             {
-                self.convoluted_input_3 : convoluted_input_3_values,
-                self.convoluted_input_4 : convoluted_input_4_values,
-                self.convoluted_input_5 : convoluted_input_5_values
+                self.convoluted_input_3 : [convoluted_input_3_values],
+                self.convoluted_input_4 : [convoluted_input_4_values],
+                self.convoluted_input_5 : [convoluted_input_5_values]
             })
             print(results) # DEBUG
         return results # DEBUG
@@ -99,48 +149,38 @@ class NeuralNetwork:
     def train(self, input_data, target_class):
         # Train the neural network given the sample
 
-        loss_hidden_layer = tf.losses.softmax_cross_entropy(target_class, self.hidden_layer_weights)
+        for i in xrange(0, len(input_data)):
+            input_data_p.append(self.__preprocess_input(input_data))
 
-
-
-        loss_convolution = {
-            3 : tf.losses.sigmoid_cross_entropy(un_truc_louche, self.kernels[3], weights = blame),
-            4 : tf.losses.sigmoid_cross_entropy(un_truc_louche, self.kernels[4], weights = blame),
-            5 : tf.losses.sigmoid_cross_entropy(un_truc_louche, self.kernels[5], weights = blame)
-        }
-
-        for i in xrange(0, self.epochs):
-            self.optimiser.minimize(self.loss_convolution[3], var_list=None, aggregation_method=tf.AggregationMethod.ADD_N, reduction=tf.losses.Reduction.SUM)
-            self.optimiser.minimize(self.loss_convolution[4], var_list=None, aggregation_method=tf.AggregationMethod.ADD_N, reduction=tf.losses.Reduction.SUM)
-            self.optimiser.minimize(self.loss_convolution[5], var_list=None, aggregation_method=tf.AggregationMethod.ADD_N, reduction=tf.losses.Reduction.SUM)
-            self.optimiser.minimize(self.loss_hidden_layer, var_list=None, aggregation_method=tf.AggregationMethod.ADD_N, reduction=tf.losses.Reduction.SUM)
+#        for i in xrange(0, self.epochs):
+#            self.optimize_step
 
         # Launch the graph
         with tf.Session() as session:
             session.run(init)
             step = 1
             # Keep training until reach max iterations
-            while step * batch_size < training_iters:
-                batch_x, batch_y = mnist.train.next_batch(batch_size)
+            while step * 50 < training_iters: # DEBUG : batch size of 50
+                #batch_x, batch_y = mnist.train.next_batch(batch_size)
+
                 # Run optimization op (backprop)
-                sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
-                                               keep_prob: dropout})
-                if step % display_step == 0:
-                    # Calculate batch loss and accuracy
-                    loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x,
-                                                                      y: batch_y,
-                                                                      keep_prob: 1.})
-                    print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
-                          "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                          "{:.5f}".format(acc))
+                sess.run(self.optimize_step, feed_dict = {input_data_p[0], target_class[0]})
+                #if step % display_step == 0:
+                #    # Calculate batch loss and accuracy
+                #    loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x,
+                #                                                      y: batch_y,
+                #                                                      keep_prob: 1.})
+                #    print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                #          "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                #          "{:.5f}".format(acc))
                 step += 1
             print("Optimization Finished!")
         
             # Calculate accuracy for 256 mnist test images
-            print("Testing Accuracy:", \
-                sess.run(accuracy, feed_dict={x: mnist.test.images[:256],
-                                              y: mnist.test.labels[:256],
-        keep_prob: 1.}))
+#            print("Testing Accuracy:", \
+#                sess.run(accuracy, feed_dict={x: mnist.test.images[:256],
+#                                              y: mnist.test.labels[:256],
+#                                              keep_prob: 1.}))
 
         return
     
--- CNN/main.py
+++ CNN/main.py
@@ -1,5 +1,7 @@
 
 import sys
+from itertools import islice
+
 import parser
 
 import numpy as np # DEBUG
@@ -27,6 +29,21 @@ def main(argv):
     ### TESTS ###
     #sentence = [[0] * 300, [1] * 300, [2] * 300, [3] * 300, [4] * 300, [5] * 300, [6] * 300]
     #sentence = np.random.random([6, 300]).astype(np.float32)
+
+    training_set_pos_length = len(data["neg"]) / 100
+    training_set_neg_length = len(data["pos"]) / 100
+
+    training_data = list(islice(data["neg"], training_set_neg_length)) + list(islice(data["pos"], training_set_pos_length))
+    training_set = []
+    for sentence in training_data:
+        training_set.append(word2vec.convertSentence(sentence))
+
+    neural_network.set_training_set(training_set,
+                                    ([[0.0, 1.0]] * training_set_neg_length) + ([[1.0, 0.0]] * training_set_pos_length),
+                                    training_set_neg_length + training_set_pos_length
+                                    )
+    #neural_network.train()
+
     sentence = ""
     while sentence != "EXIT":
         sentence = raw_input("Enter sentence : ")
